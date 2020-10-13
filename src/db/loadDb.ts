a08cdf5b26eeb36845ed6be72e9a3e41d3925cb6
Jessica Weber <jcj.weber.92@gmail.com>
2020-10-14 00:42:16 +0200

    load database config from .env

--- src/db/loadDb.ts
+++ src/db/loadDb.ts
@@ -1,5 +1,27 @@
 import {createConnection} from "typeorm";
 
 export async function loadDb() {
-    await createConnection();
+    await createConnection({
+        name: "default",
+        type: "mariadb",
+        database: process.env.DB_DATABASE,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        synchronize: true,
+        logging: false,
+        entities: [
+            "src/db/entity/*"
+        ],
+        subscribers: [
+            "src/db/subscriber/*"
+        ],
+        migrations: [
+            "src/db/migration/*"
+        ],
+        cli: {
+            entitiesDir: "src/db/entity",
+            migrationsDir: "src/db/migration",
+            subscribersDir: "src/db/subscriber"
+        }
+    });
 }
