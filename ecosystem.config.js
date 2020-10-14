8f7a95baa6890c5bbde86c6dd4b3ba3fe2f0f227
Jessica Weber <jcj.weber.92@gmail.com>
2020-10-14 11:07:02 +0200

    add ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,7 @@ module.exports = {
     apps : [{
         name: "discord_bot_tt",
         script: "src/app.ts",
-        args: "--require ts-node/register"
+        interpreter: "node",
+        node_args: "--require ts-node/register --require tsconfig-paths/register"
     }]
 };
