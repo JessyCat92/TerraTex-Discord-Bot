050c384aa6a93b76ed46f3e638bb016a1410823e
Jessica Weber <jcj.weber.92@gmail.com>
2020-10-14 18:52:59 +0200

    add time between 18-22

--- src/discord/DefaultCommands/Hello.ts
+++ src/discord/DefaultCommands/Hello.ts
@@ -9,7 +9,8 @@ registerCommand(new Command(["hallo", "hey", "hi"], async (msgObj: Message) => {
     const morning = moment().startOf("day").add(6, "h");
     const lunch = moment().startOf("day").add(12, "h");
     const afternoon = moment().startOf("day").add(14, "h");
-    const night = moment().startOf("day").add(19, "h");
+    const night = moment().startOf("day").add(18, "h");
+    const latenight = moment().startOf("day").add(22, "h");
 
     if (moment().isBefore(morning)) {
         await msgObj.reply("Hey, solltest du nicht im Bett sein? Es ist doch mitten in der Nacht...");
@@ -19,7 +20,17 @@ registerCommand(new Command(["hallo", "hey", "hi"], async (msgObj: Message) => {
         await msgObj.reply("Hey! Lunchtime! Guten Hunger! ... oder viel Spaß beim Mittagsschlaf hihi :3");
     } else if (moment().isBefore(night)) {
         await msgObj.reply("Hallo :3 Darf es ein Stück Kuchen und Kaffee sein an diesem schönen Nachmittag?");
+    } else if (moment().isBefore(latenight)) {
+        await msgObj.reply("Guten Abend :3 Lust auf eine Runde zocken?");
     } else {
         await msgObj.reply("***sing*** Guten Abend, Gute Nacht, bald wirst du ins Bett gebracht :3 ***sing***");
     }
 }));
+
+// registerCommand(new Command(["ami"], (msg) => {
+//     if(msg.mentions.roles.has(msg.member.roles.highest.id)) {
+//         // msg.member.roles.cache.has()
+//         msg.reply("Ja du hast die Rolle @" + msg.member.roles.highest.name);
+//     }
+//
+// }))
--- src/discord/DefaultCommands/index.ts
+++ src/discord/DefaultCommands/index.ts
@@ -1,2 +1,4 @@
 import "./Feiertag";
 import "./Hello";
+
+
