87b1a6a449932c2e56db7b8efda893bf9442b192
Jessica Caitlyn Weber <jcj.weber.92@gmail.com>
2021-08-08 20:27:21 +0200

    test push

--- src/discord/DefaultCommands/Help.ts
+++ src/discord/DefaultCommands/Help.ts
@@ -4,55 +4,56 @@ import {Command} from "../defintions/Command";
 import {hasPermissionLevel} from "../Permission";
 
 registerCommand(new Command(0, ["help", "hilfe"], async (msg: Message, all: string) => {
-    const cmdsSorted: {[permLevel: number]: any} = {
-        0: [], 1: [], 2: [], 3: [], 4: []
-    };
+        const cmdsSorted: { [permLevel: number]: any } = {
+            0: [], 1: [], 2: [], 3: [], 4: []
+        };
 
-    const titles = {
-        0: "General",
-        1: "Only VIP",
-        2: "Only Moderators",
-        3: "Only Administrators",
-        4: "Only Owners"
-    }
-
-    const colors = {
-        0: "GREY",
-        1: "#f805d7",
-        2: "#fd9d24",
-        3: "#ff0101",
-        4: "#c20003"
-    }
-
-    for (const indentifier in cmdList) {
-        cmdsSorted[cmdList[indentifier].permLevel].push({
-            name: cmdList[indentifier].cmds.join(", "),
-            value: cmdList[indentifier].helpDescription
-        });
-    }
-
-    for (const levelAsStr in cmdsSorted) {
-        const level = parseInt(levelAsStr, 10);
-        if (cmdsSorted[level].length === 0) continue;
-        // @ts-ignore
-        if (!await hasPermissionLevel(msg, level)) continue;
-
-        let cmds = "";
-        let desc = "";
-
-        for (const cmdObj of cmdsSorted[level]) {
-            cmds += `${cmdObj.name}\n`;
-            desc += `${cmdObj.value}\n`;
+        const titles = {
+            0: "General",
+            1: "Only VIP",
+            2: "Only Moderators",
+            3: "Only Administrators",
+            4: "Only Owners"
         }
 
-        const msgEmb = new MessageEmbed();
-        msgEmb.setTitle(titles[level]);
-        msgEmb.setColor(colors[level]);
-        msgEmb.addField("Cmds", cmds, true);
-        msgEmb.addField("Description", desc, true);
+        const colors = {
+            0: "GREY",
+            1: "#f805d7",
+            2: "#fd9d24",
+            3: "#ff0101",
+            4: "#c20003"
+        }
 
-        await msg.author.send(msgEmb);
-        await msg.reply("Ich habe dir eine DM mit allen verfügbaren Befehlen gesendet :3 UwU")
-    }
+        for (const indentifier in cmdList) {
+            cmdsSorted[cmdList[indentifier].permLevel].push({
+                name: cmdList[indentifier].cmds.join(", "),
+                value: cmdList[indentifier].helpDescription
+            });
+        }
 
-}).setDescription("Zeigt aktuelle Hilfe an"));
+        for (const levelAsStr in cmdsSorted) {
+            const level = parseInt(levelAsStr, 10);
+            if (cmdsSorted[level].length === 0) continue;
+            // @ts-ignore
+            if (!await hasPermissionLevel(msg, level)) continue;
+
+            let cmds = "";
+            let desc = "";
+
+            for (const cmdObj of cmdsSorted[level]) {
+                cmds += `${cmdObj.name}\n`;
+                desc += `${cmdObj.value}\n`;
+            }
+
+            const msgEmb = new MessageEmbed();
+            msgEmb.setTitle(titles[level]);
+            msgEmb.setColor(colors[level]);
+            msgEmb.addField("Cmds", cmds, true);
+            msgEmb.addField("Description", desc, true);
+
+            await msg.author.send(msgEmb);
+            await msg.reply("Ich habe dir eine DM mit allen verfügbaren Befehlen gesendet :3 UwU")
+        }
+    })
+    .setDescription("Zeigt aktuelle Hilfe an")
+);
