e24694d03b2937cec01c73d38af4482bebeddb00
Jessica Weber <jcj.weber.92@gmail.com>
2021-09-08 11:39:32 +0200

    test

--- src/discord/CommandHandler.ts
+++ src/discord/CommandHandler.ts
@@ -8,7 +8,7 @@ import {CommandInteraction} from "discord.js";
 class CommandHandler {
 
     public _instance = this;
-    private _cmdList: {[identifier: string]: ICommand} = {};
+    static _cmdList: {[identifier: string]: ICommand} = {};
     static _slashCmdList: {[cmdName: string]: SlashCommand} = {};
 
     constructor() {
@@ -27,10 +27,10 @@ class CommandHandler {
             const cmd = msgParts[0].slice(1).toLowerCase();
             msgParts.shift();
 
-            for (const identifier in this._cmdList) {
-                if (this._cmdList[identifier].cmds.indexOf(cmd) !== -1) {
-                    if (await hasPermissionLevel(msg, this._cmdList[identifier].permLevel)) {
-                        this._cmdList[identifier].execute(msg, ...msgParts);
+            for (const identifier in CommandHandler._cmdList) {
+                if (CommandHandler._cmdList[identifier].cmds.indexOf(cmd) !== -1) {
+                    if (await hasPermissionLevel(msg, CommandHandler._cmdList[identifier].permLevel)) {
+                        CommandHandler._cmdList[identifier].execute(msg, ...msgParts);
                     } else {
                         msg.reply("Du hast nicht die Berechtigung diesen Befehl zu nutzen!");
                     }
@@ -53,7 +53,7 @@ class CommandHandler {
      */
     public registerCommand(cmd: ICommand): string {
         const commandIdentifier = uuid();
-        this._cmdList[commandIdentifier] = cmd;
+        CommandHandler._cmdList[commandIdentifier] = cmd;
         return commandIdentifier;
     }
 
@@ -69,7 +69,7 @@ class CommandHandler {
     }
 
     get cmdList(): { [p: string]: ICommand } {
-        return this._cmdList;
+        return CommandHandler._cmdList;
     }
 
     get slashCmdList(): { [cmd: string]: SlashCommand } {
